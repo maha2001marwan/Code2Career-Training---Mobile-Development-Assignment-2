799c540252423aba7a206c515135b45af63725a5
maha2001marwan <133973693+maha2001marwan@users.noreply.github.com>
2025-08-02 17:33:44 +0300

    Create question1.dart

---
+++ question1.dart
@@ -0,0 +1,13 @@
+void main() {
+  int num1 = 2;
+  int num2 = 1;
+  
+  bool cond = (num1.isOdd && num2.isEven) || (num1 > 0 && num2 > 0);
+  
+  num1++;
+  num2--;
+  
+  int bitwiseNot = ~castedNum;
+  
+  print(num1 > num2 ? true : false);
+}
